4fa8bd94b3020223aca4a666a7427fece73b99d1
Pasindu1011 <92198044+Pasindu1011@users.noreply.github.com>
2024-01-25 09:20:09 +0530

    useEffect dependancy

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,15 +8,27 @@ const Home = () => {
     {title:'web dev top tips',body:'lorem ipsum...',author:'luigi',id:3},
   ]);
   
+  const [name,setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newblogs =blogs.filter(blog =>blog.id !== id);
     setBlogs(newblogs);
   }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+    console.log(name);
+  },[name]);
+
+  // here its only runs changename button only 
+
   return (
     <div className="home">
     <BlogList blogs={blogs} title="All Blogs!" handledelete={handleDelete}/> 
     {/* here title also an argument we pass to bloglist */}
+    <button onClick={() => setName('luigi')}>Change Name</button>
+    <p>{name}</p>
     </div>
   );
 }
