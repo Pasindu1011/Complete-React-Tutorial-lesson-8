c2b9a0116e39d0716b105c871f21ba889b02e315
Pasindu1011 <92198044+Pasindu1011@users.noreply.github.com>
2024-01-26 09:17:23 +0530

    fetch added, timeout set to 1000, ispending added to set a loading mgs untill data is fetch

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,13 +2,9 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs,setBlogs] = useState([
-    {title:'My new website',body:'lorem ipsum...',author:'mario',id:1},
-    {title:'welcome party!',body:'lorem ipsum...',author:'yoshi',id:2},
-    {title:'web dev top tips',body:'lorem ipsum...',author:'luigi',id:3},
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
   
-  const [name,setName] = useState('mario');
 
   const handleDelete = (id) => {
     const newblogs =blogs.filter(blog =>blog.id !== id);
@@ -16,19 +12,26 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-    console.log(name);
-  },[name]);
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+      });
+    },1000);
+  },[]);
 
-  // here its only runs changename button only 
+  // here its only runs change name button only 
+  //if we leave empty [] in dependency useEffect runs only once.
 
   return (
     <div className="home">
-    <BlogList blogs={blogs} title="All Blogs!" handledelete={handleDelete}/> 
-    {/* here title also an argument we pass to bloglist */}
-    <button onClick={() => setName('luigi')}>Change Name</button>
-    <p>{name}</p>
+      {isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} title='All blogs!' handledelete={handleDelete} /> }
+      
     </div>
   );
 }
