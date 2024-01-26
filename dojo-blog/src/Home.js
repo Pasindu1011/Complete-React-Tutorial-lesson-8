158b7986d90c9e0235bfff98e7776b3898a047ad
Pasindu1011 <92198044+Pasindu1011@users.noreply.github.com>
2024-01-26 20:39:18 +0530

    Make a custom hook for reuse when url is different

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,48 +1,16 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
 
-  //we create a usestate to set error mgs and to display that error
-
-  const [error, setError] = useState(null);
+  const {data:blogs, isPending ,error } = useFetch('http://localhost:8000/blogs');
   
-
-  const handleDelete = (id) => {
-    const newblogs =blogs.filter(blog =>blog.id !== id);
-    setBlogs(newblogs);
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        if(!res.ok){
-          throw Error('cloud not fetch the data for that resource');
-        }
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-      })
-      .catch(err => {
-        setIsPending(false);
-        console.log(err.message);
-      })
-    },1000);
-  },[]);
-
-  // here its only runs change name button only 
-  //if we leave empty [] in dependency useEffect runs only once.
-
   return (
     <div className="home">
       {error && <div>{error} </div>}
       {isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} title='All blogs!' handledelete={handleDelete} /> }
+      {blogs && <BlogList blogs={blogs} title='All blogs!' /> }
       
     </div>
   );
